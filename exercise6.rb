7b76726f3a2e9f4133394604eb57546b874d0c07
dmmanns <dmmanns555@gmail.com>
2015-10-28 17:52:34 -0400

    Exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,27 @@
-def additional(groceries) 
-   (grocery_list) <<
+def add_item(list, new_item) 
+    list << new_item 
+
 end
 
-puts grocery_list = ["fruit", "vegtables", "cereal", "yogut", "cheese", "milk"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+add_item(grocery_list, "rice")
+grocery_list.each_with_index do |item, index|
+	grocery_list [index] = "*#{item}"
+
+end
+
+puts grocery_list.sort
+puts "There are #{grocery_list.count} items to be picked up today"
+puts "What are you looking for on your list?"
+looking_for = gets.chomp
+if grocery_list.include?("looking_for") 
+	puts "You don't need to pick-up #{looking_for} today"
+else puts "You need to pick-up #{looking_for}"
+
+end
+
+grocery_list.pop
+
+puts grocery_list[1]
 
---
+++ exercise7.rb
@@ -0,0 +1,12 @@
+def  display_cohort(name, number_)
+	puts students 
+end
+
+students = {
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
+
+puts students
+
