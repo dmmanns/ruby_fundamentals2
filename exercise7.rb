d74a9cac756839d463598b2c957c177c4332e47d
dmmanns <dmmanns555@gmail.com>
2015-10-28 19:16:00 -0400

    Exercise 7 Q 4,5

--- exercise7.rb
+++ exercise7.rb
@@ -3,9 +3,12 @@
 def  display_cohort(list)
 	list.each do |name, number|
 		puts "#{name}: #{number} students"
+		#number 4
+		puts number * 1.05.to_i
 	end
 
 end
+
 
 students = {
 	:cohort1 => 34,
@@ -20,6 +23,22 @@ list = display_cohort(students)
 
 #number3
 puts list.keys
+
+#number5
+puts "********"
+students.delete:cohort2
+puts "********"
+
+list = display_cohort(students)
+puts list
+
+#BONUS
+def total_students(sum)
+	sum.each do |name, number|
+	puts "#{number}" 
+ end
+
+end
 
 
 
